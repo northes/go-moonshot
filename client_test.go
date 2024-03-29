ada1ecbe087dc1447c9fcc4099d6cb60d1aea882
northes <me@northes.io>
2024-03-29 12:05:24 +0800

    test(client): test empty config

--- client_test.go
+++ client_test.go
@@ -3,16 +3,28 @@ package moonshot_test
 import (
 	"errors"
 	"os"
+	"testing"
 
 	"github.com/northes/go-moonshot"
+	"github.com/stretchr/testify/require"
 )
 
 func NewTestClient() (*moonshot.Client, error) {
-	key, ok := os.LookupEnv("moonshot_key")
+	key, ok := os.LookupEnv("MOONSHOT_KEY")
 	if !ok {
-		return nil, errors.New("missing environment variable: moonshot_key")
+		return nil, errors.New("missing environment variable: MOONSHOT_KEY")
 	}
 	return moonshot.NewClient(moonshot.NewConfig(
 		moonshot.SetAPIKey(key),
 	))
+}
+
+func TestConfig(t *testing.T) {
+	tt := require.New(t)
+
+	_, err := moonshot.NewClient(nil)
+	tt.NotNil(err, "must got a required api key error")
+
+	_, err = moonshot.NewClient(moonshot.NewConfig())
+	tt.NotNil(err, "must got a required api key error")
 }
