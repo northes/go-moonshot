3796ea39a377f5040ef6002b9fee853163ac5a4d
northes <me@northes.io>
2024-05-09 15:27:29 +0800

    feat(builder/chat_completions): annotation of the method

--- chat_completions_builder.go
+++ chat_completions_builder.go
@@ -24,6 +24,7 @@ type chatCompletionsBuilder struct {
 	req *ChatCompletionsRequest
 }
 
+// NewChatCompletionsBuilder creates a new chat completions builder, or with the given request
 func NewChatCompletionsBuilder(req ...*ChatCompletionsRequest) IChatCompletionsBuilder {
 	builder := &chatCompletionsBuilder{
 		req: &ChatCompletionsRequest{},
@@ -47,6 +48,7 @@ func (c *chatCompletionsBuilder) preCheck() {
 	}
 }
 
+// AddUserContent add a message with the role of user
 func (c *chatCompletionsBuilder) AddUserContent(content string) IChatCompletionsBuilder {
 	c.req.Messages = append(c.req.Messages, &ChatCompletionsMessage{
 		Role:    RoleUser,
@@ -55,6 +57,7 @@ func (c *chatCompletionsBuilder) AddUserContent(content string) IChatCompletions
 	return c
 }
 
+// AddSystemContent add a message with the role of system
 func (c *chatCompletionsBuilder) AddSystemContent(content string) IChatCompletionsBuilder {
 	c.req.Messages = append(c.req.Messages, &ChatCompletionsMessage{
 		Role:    RoleSystem,
@@ -63,6 +66,7 @@ func (c *chatCompletionsBuilder) AddSystemContent(content string) IChatCompletio
 	return c
 }
 
+// AddAssistantContent adds a message with the role of assistant
 func (c *chatCompletionsBuilder) AddAssistantContent(content string) IChatCompletionsBuilder {
 	c.req.Messages = append(c.req.Messages, &ChatCompletionsMessage{
 		Role:    RoleAssistant,
@@ -71,60 +75,72 @@ func (c *chatCompletionsBuilder) AddAssistantContent(content string) IChatComple
 	return c
 }
 
+// AddPrompt is an alias of AddSystemContent
 func (c *chatCompletionsBuilder) AddPrompt(prompt string) IChatCompletionsBuilder {
 	return c.AddSystemContent(prompt)
 }
 
+// AddMessage add ChatCompletionsMessage to the request
 func (c *chatCompletionsBuilder) AddMessage(message *ChatCompletionsMessage) IChatCompletionsBuilder {
 	c.req.Messages = append(c.req.Messages, message)
 	return c
 }
 
+// SetModel sets the model of the request
 func (c *chatCompletionsBuilder) SetModel(model ChatCompletionsModelID) IChatCompletionsBuilder {
 	c.req.Model = model
 	return c
 }
 
+// SetTemperature sets the temperature of the request
 func (c *chatCompletionsBuilder) SetTemperature(temperature float64) IChatCompletionsBuilder {
 	c.req.Temperature = temperature
 	return c
 }
 
+// SetMaxTokens sets the max tokens of the request
 func (c *chatCompletionsBuilder) SetMaxTokens(num int) IChatCompletionsBuilder {
 	c.req.MaxTokens = num
 	return c
 }
 
+// SetTopP sets the top p of the request
 func (c *chatCompletionsBuilder) SetTopP(num float64) IChatCompletionsBuilder {
 	c.req.TopP = num
 	return c
 }
 
+// SetN sets the n of the request
 func (c *chatCompletionsBuilder) SetN(num int) IChatCompletionsBuilder {
 	c.req.N = num
 	return c
 }
 
+// SetPresencePenalty sets the presence penalty of the request
 func (c *chatCompletionsBuilder) SetPresencePenalty(num float64) IChatCompletionsBuilder {
 	c.req.PresencePenalty = num
 	return c
 }
 
+// SetFrequencyPenalty sets the frequency penalty of the request
 func (c *chatCompletionsBuilder) SetFrequencyPenalty(num float64) IChatCompletionsBuilder {
 	c.req.FrequencyPenalty = num
 	return c
 }
 
+// SetStop sets the stop of the request
 func (c *chatCompletionsBuilder) SetStop(stop []string) IChatCompletionsBuilder {
 	c.req.Stop = stop
 	return c
 }
 
+// SetStream sets the stream of the request
 func (c *chatCompletionsBuilder) SetStream() IChatCompletionsBuilder {
 	c.req.Stream = true
 	return c
 }
 
+// ToRequest returns the ChatCompletionsRequest
 func (c *chatCompletionsBuilder) ToRequest() *ChatCompletionsRequest {
 	return c.req
 }
